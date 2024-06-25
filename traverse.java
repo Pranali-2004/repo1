f6b82856ebd398fc324d26378cc5fa51f9e551c8
Sayali <abc@gmail.com>
2024-06-25 11:01:24 +0530

    first commit done...

---
+++ traverse.java
@@ -0,0 +1,12 @@
+class Traverse
+{
+  public static void main(String args[])
+  {
+    int array[]={4,6,7,3,2,8};
+    int i;
+    for(i=0;i<array.length;i++)
+    {
+      System.out.println(array[i]);
+    }
+  }
+}
